1f857cd2c0db6fa55ed2a1f4abf0d1b2dc245a34
Chethan D A <237268008+Chethan-D-A@users.noreply.github.com>
2026-06-19 11:11:01 +0530

    Update rename.py

--- tests/rename.py
+++ tests/rename.py
@@ -1,9 +1,7 @@
-# aathman/tests/rename.py
 import torch
 
 sd = torch.load("model.pth", map_location="cpu")
 keys = sorted(sd.keys())
-# Rename the first key deterministically
 old = keys[0]
 sd["renamed::" + old] = sd.pop(old)
 torch.save(sd, "model_renamed.pth")
