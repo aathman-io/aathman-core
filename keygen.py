c6ffaf2486ae44c61446773129ffbfc636e974e3
Chethan D A <237268008+Chethan-D-A@users.noreply.github.com>
2026-06-19 11:12:54 +0530

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -1,18 +1,14 @@
-# aathman/keygen.py
 from nacl.signing import SigningKey
 from pathlib import Path
 import os
 
-# Keys stored in your home directory (ultra secure location)
 KEY_DIR = Path.home() / ".aathman" / "keys"
 KEY_DIR.mkdir(parents=True, exist_ok=True)
 
 def main():
-    # Generate Ed25519 keypair
     sk = SigningKey.generate()
     pk = sk.verify_key
 
-    # Save private and public keys
     (KEY_DIR / "private.key").write_bytes(sk.encode())
     (KEY_DIR / "public.key").write_bytes(pk.encode())
 
